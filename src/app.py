0e054f0c42b04b324f7beeaf229dbd8cb2aae27c
zplunte <zpl@uoregon.edu>
2017-02-12 22:40:26 -0800

    Add red and lost_key routes

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from config import dbname, dbhost, dbport
+import json, datetime, time
 
 # NOTE: Couldn't quite figure out how to include database info...
 # I know I need to do something with the psql database in fir()
@@ -36,6 +37,29 @@ def itr():
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
+
+@app.route('/rest')
+def rest():
+    return render_template('rest.html')
+
+@app.route('/rest/lost_key', methods=(['POST']))
+def lost_key():
+
+    # Create dictionary for response data
+    response_data = dict()
+
+    # Get formatted timestamp, create key-value pair in dictionary
+    timest = time.time()
+    response_data["timestamp"] = datetime.datetime.fromtimestamp(timest).strftime("%Y-%m-%d %H:%M%:S")
+
+    # Determine result
+    response_data["result"] = "OK"
+
+    # Provide LOST public key
+    response_data["key"] = "key123key456key789..."
+
+    # return response_data via json
+    return json.dumps(response_data)
 
 if __name__ == "__main__":
     app.run()
