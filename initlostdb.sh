aa72fad0a68d42f8e27b5c0cbe031d927320593e
zplunte <zpl@uoregon.edu>
2017-02-26 19:42:53 -0800

    created script for db init

---
+++ initlostdb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# A bash script for initializing the LOST db in the OSNAP env 
+
+# Initialize the database under $HOME
+initdb -D $HOME/data
+
+# Start running database server, create logfile
+pg_ctl -D $HOME/data -l logfile start
+
+# Create the database 'lost'
+createdb lost
