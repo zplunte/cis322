6a34fe3142e76eba7dabeba2faa78452dcb20ecb
zplunte <zpl@uoregon.edu>
2017-02-26 20:29:46 -0800

    Assignment7 table updates

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,13 +1,38 @@
 /* Creates the necessary database tables for LOST web application. */
 
-/* NOTE: Here I chose to use the username field as the primary key 
-directly. I don't have much experience managing primary/foreign keys 
-and this seems to make the most sense right now. I chose length 16 
-for both the username and password fields because this forces the 
-user to choose usernames and passwords that are at most 16 characters 
-long. */ 
-
+/* A user can have a username, a password, and an associated role */
 CREATE TABLE userdata (
     username varchar(16),
-    password varchar(16)
+    password varchar(16),
+    role_fk int REFERENCES roles (role_pk)
+);
+
+/* Decided to create a new table specifically for roles */
+CREATE TABLE roles (
+    role_pk serial primary key,
+    role varchar(256)
+);
+
+/* Assets table with primary key, tag, description, disposed flag, and in transit flag */
+CREATE TABLE assets (
+    asset_pk serial primary key,
+    asset_tag varchar(16),
+    description varchar(256),
+    is_disposed boolean default false,
+    in_transit boolean default false
+);
+
+/* Facilities table with primary key, common name, and facility code */
+CREATE TABLE facilities (
+    facility_pk serial primary key,
+    common_name varchar(32),
+    code varchar(6)
+);
+
+/* Decided to create an extra table for asset/facility relationship data */
+CREATE TABLE asset_position (
+    arrival_time date,
+    departure_time date,
+    asset_fk int REFERENCES assets (asset_pk),
+    facility_fk int REFERENCES facilities (facility_pk)
 );
