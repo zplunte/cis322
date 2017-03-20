d2af997582ed8916101db21be236ef4b8fb92381
zplunte <zpl@uoregon.edu>
2017-03-20 00:40:23 -0700

    added clients add/revoke user

---
+++ clients/activate_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('losturl', type=str)
+    parser.add_argument('username', type=str)
+    parser.add_argument('password', type=str)
+    parser.add_argument('role', type=str)
+    args = parser.parse_args()
+    losturl = args.losturl
+
+    data = dict()
+    data['username'] = args.username
+    data['password'] = args.password
+    data['role'] = args.role
+    
+    json_data = dict()
+    json_data['arguments'] = json.dumps(data)
+    send_data = urlencode(json_data)
+
+    post_request = Request(losturl, send_data.encode('ascii'), method='POST')
